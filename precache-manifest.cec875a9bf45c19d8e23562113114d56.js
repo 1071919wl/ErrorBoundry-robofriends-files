160d28fdbcb89bec8ba390e23b5e79c83daa2c50
William Leung <Wllm@Williams-MacBook-Pro.local>
2020-05-04 17:55:12 -0700

    Updates

---
+++ precache-manifest.cec875a9bf45c19d8e23562113114d56.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c56d6ef12578c2b4c846e46527f92f0d",
+    "url": "/ErrorBoundry-robofriends-files/index.html"
+  },
+  {
+    "revision": "155520c129548449f706",
+    "url": "/ErrorBoundry-robofriends-files/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "e84a6caa4bd940ea83c3",
+    "url": "/ErrorBoundry-robofriends-files/static/css/main.47d564b2.chunk.css"
+  },
+  {
+    "revision": "155520c129548449f706",
+    "url": "/ErrorBoundry-robofriends-files/static/js/2.d82cd422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ErrorBoundry-robofriends-files/static/js/2.d82cd422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e84a6caa4bd940ea83c3",
+    "url": "/ErrorBoundry-robofriends-files/static/js/main.9346620f.chunk.js"
+  },
+  {
+    "revision": "29c4827fb8cd5eb916ad",
+    "url": "/ErrorBoundry-robofriends-files/static/js/runtime-main.6ae3dc55.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ErrorBoundry-robofriends-files/static/media/SEGA.12a3db8b.woff"
+  }
+]);
